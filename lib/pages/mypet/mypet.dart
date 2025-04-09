9bf470e155352df45fd3e4c1cde8d3fe0e6b4abf
thucanh <hathucanhls2004@gmail.com>
2025-04-09 22:53:49 +0700

    ok

--- lib/pages/mypet/mypet.dart
+++ lib/pages/mypet/mypet.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
 
-StatefulWidget 
+class MyPet extends StatefulWidget {
+  const MyPet({super.key});
+
+  @override
+  State<MyPet> createState() => _MyPetState();
+}
+
+class _MyPetState extends State<MyPet> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Pet')),
+      body: Center(child: const Text('My Pet Page')),
+    );
+  }
+}
