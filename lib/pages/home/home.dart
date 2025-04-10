c2f0c2880f415e04a06e4cc1f251267774065fdf
thucanh <hathucanhls2004@gmail.com>
2025-04-10 10:38:27 +0700

    overflowbug fixed

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -54,7 +54,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
       appBar: _appBar(),
       body: SafeArea(
         child: Padding(
